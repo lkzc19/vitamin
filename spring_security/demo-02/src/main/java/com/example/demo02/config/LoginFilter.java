694505baba927a7af644f0ada28a09f84b3318ae
lkzc19 <lkzc19@foxmail.com>
2024-03-13 22:56:59 +0800

    feat: copy from demo-01

---
+++ spring_security/demo-02/src/main/java/com/example/demo02/config/LoginFilter.java
@@ -0,0 +1,45 @@
+package com.example.demo02.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * 自定义前后端分离认证 Filter
+ */
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        // 1. 判断是否是 POST 请求
+        if (!request.getMethod().equals("POST")) {
+            throw new AuthenticationServiceException("Authentication method not supported: " + request.getMethod());
+        }
+        // 2. 判断是否是 json 格式类型
+        if (request.getContentType().equalsIgnoreCase(MediaType.APPLICATION_JSON_VALUE)) {
+            // 3. 从json数据中获取用户输入用户名和密码进行认证
+            try {
+                Map<String, String> userInfo = new ObjectMapper().readValue(request.getInputStream(), Map.class);
+                String username = userInfo.get(getUsernameParameter());
+                String password = userInfo.get(getPasswordParameter() );
+                System.out.println(username + ": " + password);
+                UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(username, password);
+                setDetails(request, authRequest);
+                return this.getAuthenticationManager().authenticate(authRequest);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return super.attemptAuthentication(request, response);
+    }
+}
--- spring_security/demo-02/src/main/java/com/example/demo02/config/WebSecurityConfig.java
+++ spring_security/demo-02/src/main/java/com/example/demo02/config/WebSecurityConfig.java
@@ -1,19 +1,99 @@
 package com.example.demo02.config;
 
-import com.example.demo02.controller.handler.DemoLogoutSuccessHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+        inMemoryUserDetailsManager.createUser(User.withUsername("root").password("{noop}123").roles("admin").build());
+        return inMemoryUserDetailsManager;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Bean
+    public LoginFilter loginFilter() throws Exception {
+        LoginFilter loginFilter = new LoginFilter();
+        loginFilter.setFilterProcessesUrl("/doLogin");
+        loginFilter.setUsernameParameter("username");
+        loginFilter.setPasswordParameter("password");
+        loginFilter.setAuthenticationManager(authenticationManagerBean());
+        // 认证成功
+        loginFilter.setAuthenticationSuccessHandler((request, response, authentication) -> {
+            Map<String, Object> result = new HashMap<>();
+            result.put("message", "登入成功: " + authentication);
+            response.setStatus(HttpStatus.OK.value());
+            response.setContentType("application/json;charset=UTF-8");
+            String s = new ObjectMapper().writeValueAsString(result);
+            response.getWriter().println(s);
+        });
+        // 认证失败
+        loginFilter.setAuthenticationFailureHandler((request, response, exception) -> {
+            Map<String, Object> result = new HashMap<>();
+            result.put("message", "登入失败: " + exception.getMessage());
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            response.setContentType("application/json;charset=UTF-8");
+            String s = new ObjectMapper().writeValueAsString(result);
+            response.getWriter().println(s);
+        });
+        return loginFilter;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .anyRequest().authenticated()
-                .and();
+                .and()
+                .formLogin()
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint((request, response, authException) -> {
+                    response.setContentType("application/json;charset=UTF-8");
+                    response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                    response.getWriter().println("请认证之后再处理");
+                })
+                .and()
+                .logout()
+                .logoutSuccessHandler((request, response, authentication) -> {
+                    Map<String, Object> result = new HashMap<>();
+                    result.put("message", "注销成功, 当前的认证对象为: " + authentication);
+                    response.setContentType("application/json;charset=UTF-8");
+                    String s = new ObjectMapper().writeValueAsString(result);
+                    response.getWriter().println(s);
+                })
+                .and()
+                .csrf().disable();
+
+        // at 替换
+        // before 之前
+        // after 之后
+        http.addFilterAt(loginFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
