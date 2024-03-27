af59825ff14084df752f968e9d0ac1b6ed82442b
lkzc19 <lkzc19@foxmail.com>
2024-03-27 17:20:22 +0800

    feat: ws

--- goy/gin-nhooyr/main.go
+++ goy/gin-nhooyr/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
@@ -12,44 +11,43 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintln(w, "HTTP, Hello")
-	})
-	http.HandleFunc("/ws", func(w http.ResponseWriter, req *http.Request) {
-		conn, err := websocket.Accept(w, req, nil)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		defer conn.Close(websocket.StatusInternalError, "内部出错了")
-		ctx, cancel := context.WithTimeout(req.Context(), time.Second*10)
-		defer cancel()
-
-		var v interface{}
-		err = wsjson.Read(ctx, conn, &v)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		log.Printf("接收到客户端: %v\n", v)
-
-		err = wsjson.Write(ctx, conn, "Hello WebSocket Client")
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		conn.Close(websocket.StatusNormalClosure, "")
-	})
-	log.Fatal(http.ListenAndServe(":2021", nil))
-
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+	r.GET("/ws", websocketHandler)
 	err := r.Run()
 	if err != nil {
 		return
 	} // 监听并在 0.0.0.0:8080 上启动服务
+}
+
+func websocketHandler(c *gin.Context) {
+	// 将HTTP连接升级为WebSocket连接
+	conn, err := websocket.Accept(c.Writer, c.Request, nil)
+	if err != nil {
+		// 处理错误
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+	defer conn.Close(websocket.StatusInternalError, "内部出错了")
+	ctx, cancel := context.WithTimeout(c.Request.Context(), time.Second*10)
+	defer cancel()
+
+	var v interface{}
+	err = wsjson.Read(ctx, conn, &v)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Printf("接收到客户端: %v\n", v)
+
+	err = wsjson.Write(ctx, conn, "Hello WebSocket Client")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	conn.Close(websocket.StatusNormalClosure, "")
 }
