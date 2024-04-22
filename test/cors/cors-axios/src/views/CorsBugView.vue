8cdcde81178835670d9bcd9f08913ebd2452e5f0
lkzc19 <lkzc19@foxmail.com>
2024-04-22 23:09:23 +0800

    feat: cors 测试

---
+++ test/cors/cors-axios/src/views/CorsBugView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {onMounted, ref} from "vue";
+import { ping } from "@/api/cors.js"
+
+const el = ref("")
+
+onMounted(async () => {
+  const pong = await ping();
+  el.value = pong.data
+})
+</script>
+
+<template>
+  <div class="about">
+    <h1>ping</h1>
+    <p>{{ el }}</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ test/cors/cors-axios/src/views/PingView.vue
@@ -0,0 +1,24 @@
+<script setup>
+  import { ping } from "@/api/cors.js"
+
+  async function ping() {
+    const pong = await ping();
+    console.log(pong)
+  }
+</script>
+
+<template>
+  <div class="about">
+    <h1></h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
