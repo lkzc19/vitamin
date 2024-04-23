309fae2344c8ff2396c04b1eec098ab28450b7c6
lkzc19 <lkzc19@foxmail.com>
2024-04-23 22:50:18 +0800

    feat: cors 测试

---
+++ test/cors/cors-axios/src/views/XHutao.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {onMounted, ref} from "vue";
+import {bar} from "@/api/cors.js"
+
+const el = ref("")
+
+onMounted(async () => {
+  const r = await bar({name: "携带 Authorization"});
+  el.value = r.data
+})
+</script>
+
+<template>
+  <div class="about">
+    <h1>携带 Authorization</h1>
+    <p>{{ el }}</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column; /* 设置为垂直方向排列 */
+    align-items: center;
+    justify-content: center; /* 垂直居中对齐 */
+  }
+}
+</style>
---
+++ test/cors/cors-axios/src/views/XNahidaView.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {onMounted, ref} from "vue";
+import {foo1} from "@/api/cors.js"
+
+const el = ref("")
+
+onMounted(async () => {
+  const r = await foo1({name: "携带请求头 X-Nahida"});
+  el.value = r.data
+})
+</script>
+
+<template>
+  <div class="about">
+    <h1>携带请求头 X-Nahida</h1>
+    <p>{{ el }}</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column; /* 设置为垂直方向排列 */
+    align-items: center;
+    justify-content: center; /* 垂直居中对齐 */
+  }
+}
+</style>
