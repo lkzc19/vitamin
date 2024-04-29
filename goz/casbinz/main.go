29b7bf4ae7559941b7548bda8888d04f97b945ea
lkzc19 <lkzc19@foxmail.com>
2024-04-29 17:26:38 +0800

    feat: casbin

--- goz/casbinz/main.go
+++ goz/casbinz/main.go
@@ -26,25 +26,45 @@ func init() {
 }
 
 func main() {
-
+	err := e.LoadPolicy()
+	checkErr(err)
 	f := func() {
-		err := e.LoadPolicy()
-		checkErr(err)
-
-		testBigData()
-
-		err = e.SavePolicy()
-		checkErr(err)
+		testBigData2()
 	}
+	err = e.SavePolicy()
+	checkErr(err)
 
 	t(f)
 }
 
 // testBigData 测试 casbin gorm 插入大量数据
+// 使用 AddPolicy
 func testBigData() {
-	for i := 0; i < 50000; i++ {
-		// 相同的策略 casbin 内部会做去重
-		_, err := e.AddPolicy("bar"+strconv.Itoa(i), "data1", "read")
+	for i := 0; i < 200000; i++ {
+		_, err := e.AddPolicy("million"+strconv.Itoa(i), "data1", "read")
+		checkErr(err)
+	}
+}
+
+// testBigData 测试 casbin gorm 插入大量数据
+// 使用 AddPolicy
+func testBigData2() {
+	pSet := make([][][]string, 0)
+
+	p := make([][]string, 0)
+	for i := 0; i < 200000; i++ {
+		if len(p) == 1000 {
+			pSet = append(pSet, p)
+			p = make([][]string, 0)
+		}
+		p = append(p, []string{"millionx" + strconv.Itoa(i), "data1", "read"})
+	}
+	if len(p) > 0 {
+		pSet = append(pSet, p)
+	}
+
+	for _, it := range pSet {
+		_, err := e.AddPolicies(it)
 		checkErr(err)
 	}
 }
