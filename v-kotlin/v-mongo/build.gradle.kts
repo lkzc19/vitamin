5519dacb385584ddcd764e32b64c35b3a6ee262b
lkzc19 <lkzc19@foxmail.com>
2024-05-15 16:47:27 +0800

    feat: v-mongo

---
+++ v-kotlin/v-mongo/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.mongodb:mongodb-driver-kotlin-sync:5.0.0")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
