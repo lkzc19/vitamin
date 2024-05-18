8686a168ff325fb7d45634c2ac6720e1753188b4
lkzc19 <lkzc19@foxmail.com>
2024-05-18 18:22:33 +0800

    feat: v-mongo gradle

--- v-kotlin/v-mongo/README.md
+++ v-kotlin/v-mongo/README.md
@@ -2,3 +2,4 @@
 
 [Kotlin 驱动程序快速入门](https://www.mongodb.com/zh-cn/docs/drivers/kotlin/coroutine/current/quick-start/)
 
+[gradle 依赖统一管理](https://docs.gradle.org/current/userguide/platforms.html)
--- v-kotlin/v-mongo/settings.gradle.kts
+++ v-kotlin/v-mongo/settings.gradle.kts
@@ -1,3 +1,5 @@
+rootProject.name = "v-mongo"
+
 pluginManagement {
     repositories {
         mavenCentral()
@@ -9,4 +11,23 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 
-rootProject.name = "v-mongo"
+dependencyResolutionManagement {
+    versionCatalogs {
+        // mongo
+        create("mongo") {
+            val mongo = version("mongo", "5.1.0")
+            library("driver", "org.mongodb", "mongodb-driver-kotlin-coroutine").versionRef(mongo)
+            library("bson", "org.mongodb", "bson-kotlinx").versionRef(mongo)
+        }
+        
+        // log
+        create("log") {
+            val slf4j = version("slf4j", "1.7.30")
+            library("slf4j", "org.slf4j", "slf4j-api").versionRef(slf4j)
+            
+            val logback = version("ch.qos.logback", "1.2.11")
+            library("logback", "ch.qos.logback", "logback-classic").versionRef(logback)
+        }
+    }
+}
+
