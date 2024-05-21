bf40a1d50da384230385eeceeb0aeee9f7d4007a
lkzc19 <lkzc19@foxmail.com>
2024-05-22 00:42:03 +0800

    feat: python sqlite3 docker 测试

--- v-python/v-sqlite3/main.py
+++ v-python/v-sqlite3/main.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     c.execute('CREATE TABLE IF NOT EXISTS v_foo(message text)')
     c.execute('INSERT INTO v_foo (message) VALUES (\'Hello\')')
     c.execute('INSERT INTO v_foo (message) VALUES (\'World\')')
+    connection.commit()
     c.execute('SELECT * FROM v_foo')
     data = c.fetchall()
     print(data)
