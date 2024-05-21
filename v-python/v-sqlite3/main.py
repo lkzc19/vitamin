e5eda5e2d05e0cc3786e6136b0e5a983d05bc9de
lkzc19 <lkzc19@foxmail.com>
2024-05-22 00:08:37 +0800

    feat: python sqlite3 docker 测试

--- v-python/v-sqlite3/main.py
+++ v-python/v-sqlite3/main.py
@@ -2,7 +2,7 @@ import sqlite3
 
 
 if __name__ == '__main__':
-    connection = sqlite3.connect('vitamin.db')
+    connection = sqlite3.connect('db/vitamin.db')
     c = connection.cursor()
     c.execute('CREATE TABLE IF NOT EXISTS v_foo(message text)')
     c.execute('INSERT INTO v_foo (message) VALUES (\'Hello\')')
