4f80d652f7d95b1264e83342ab83a6e943c5f2a1
lkzc19 <lkzc19@foxmail.com>
2024-05-28 22:06:34 +0800

    feat: upload test

---
+++ v-demo/upload/upload-spring/src/main/kotlin/org/example/upload/UploadSpringApplication.kt
@@ -0,0 +1,11 @@
+package org.example.upload
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UploadSpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<UploadSpringApplication>(*args)
+}
---
+++ v-demo/upload/upload-spring/src/test/kotlin/org/example/upload/UploadSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.upload
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UploadSpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
