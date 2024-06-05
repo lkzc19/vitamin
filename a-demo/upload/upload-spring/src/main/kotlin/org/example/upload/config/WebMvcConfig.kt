0ecefbd026a9208b50890a41bd6d9be67cdcffb4
lkzc19 <lkzc19@foxmail.com>
2024-06-05 23:14:15 +0800

    feat: 跨域处理

--- a-demo/upload/upload-spring/src/main/kotlin/org/example/upload/config/WebMvcConfig.kt
+++ a-demo/upload/upload-spring/src/main/kotlin/org/example/upload/config/WebMvcConfig.kt
@@ -1,18 +1,20 @@
 package org.example.upload.config
 
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-/**
- * 使用配置的方式设置静态文件位置
- */
-//@Configuration
-class WebMvcConfig : WebMvcConfigurationSupport() {
-
-    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
-        //将所有F:/resources/目录下的资源,访问时都映射到/res/** 路径下
+@Configuration
+class WebMvcConfig : WebMvcConfigurer {
+    
+    override fun addCorsMappings(registry: CorsRegistry) {
         registry
-            .addResourceHandler("/file/**")
-            .addResourceLocations("file:C:/tmp/")
+            .addMapping("/**")
+//            .allowCredentials(true) // 设置allowCredentials为true后，allowedOrigins就不能使用*，可以使用allowedOriginPatterns做通配
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*")
     }
 }
