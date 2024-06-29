20dabfc481b0248e0b44ca62b9af25835642ef2d
lkzc19 <lkzc19@foxmail.com>
2024-06-30 00:03:26 +0800

    docs: 设计模式 适配器模式

--- v-langx/x-gof/gof-kotlin/src/main/kotlin/adapter/Adapter.kt
+++ v-langx/x-gof/gof-kotlin/src/main/kotlin/adapter/Adapter.kt
@@ -2,6 +2,13 @@ package adapter
 
 import kotlin.math.sqrt
 
+/**
+ * 不要为了使用设计模式而去使用设计模式
+ * 适配器是为了`不修改`或是想`复用`原先代码的情况下而被迫使用的一种设计模式
+ *
+ * 比如想要复用 RoundHole.fits 的逻辑 且不想修改 那么就使用适配器
+ */
+
 // 圆孔
 class RoundHole(private val radius: Int) {
     fun fits(peg: RoundPeg) = radius >= peg.radius
