26e7366f6f41584e1bfb0c3d2d23ad85ef690b34
lkzc19 <lkzc19@foxmail.com>
2024-07-11 16:09:19 +0800

    feat: go json

--- v-lang/l-go/v-json/main.go
+++ v-lang/l-go/v-json/main.go
@@ -3,18 +3,30 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 )
 
 func main() {
-	jsonStr := "{\"nahida\":\"纳西妲\",\"hutao\":{\"first\":\"hu\",\"second\":\"tao\"}}"
+	jsonStr := `{
+		"nahida": "纳西妲",
+		"hutao": {
+			"first": "hu",
+			"second": "tao"
+		},
+		"time": 6.00005
+	}`
 	var data map[string]any
 	err := json.Unmarshal([]byte(jsonStr), &data)
 	checkErr(err)
 
+	fmt.Println(jsonStr)
 	fmt.Println(data)
 	hutao := data["hutao"].(map[string]any)
 	fmt.Println(hutao)
 	fmt.Println(hutao["first"])
+
+	of := reflect.TypeOf(data["time"])
+	fmt.Println(of)
 }
 
 func checkErr(err error) {
