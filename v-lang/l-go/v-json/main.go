fba9f93300ddb849df21f21d758acd39a17d9afa
lkzc19 <lkzc19@foxmail.com>
2024-07-11 11:29:12 +0800

    feat: go json

--- v-lang/l-go/v-json/main.go
+++ v-lang/l-go/v-json/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("hello world")
+	jsonStr := "{\"nahida\":\"纳西妲\",\"hutao\":{\"first\":\"hu\",\"second\":\"tao\"}}"
+	var data map[string]any
+	err := json.Unmarshal([]byte(jsonStr), &data)
+	checkErr(err)
+
+	fmt.Println(data)
+	hutao := data["hutao"].(map[string]any)
+	fmt.Println(hutao)
+	fmt.Println(hutao["first"])
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
