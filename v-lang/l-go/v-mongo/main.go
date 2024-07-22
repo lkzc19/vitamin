aaff1293ba036d23bfaaed687ed1030ef63999f0
lkzc19 <lkzc19@foxmail.com>
2024-07-22 10:17:05 +0800

    feat: go mongo

--- v-lang/l-go/v-mongo/main.go
+++ v-lang/l-go/v-mongo/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"v-mongo/internal"
+)
 
+var db *mongo.Database
+
+func init() {
+	uri := "mongodb://localhost:23017"
+	dbName := "vitamin"
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(uri))
+	internal.CheckErr(err)
+	err = client.Ping(context.Background(), readpref.Primary())
+	internal.CheckErr(err)
+	db = client.Database(dbName)
+}
+
+func main() {
+	fmt.Println(db.Name())
 }
