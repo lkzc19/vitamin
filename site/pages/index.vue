9fa6bc827bc1d5e38396b0bb1e9d63bb230402ff
lkzc19 <lkzc19@foxmail.com>
2024-07-23 21:40:04 +0800

    feat: 前端真难

--- site/pages/index.vue
+++ site/pages/index.vue
@@ -1,9 +1,48 @@
 <script setup lang="ts">
-const route = useRoute()
+
 </script>
 
 <template>
-  <header>111</header>
-  <main>222</main>
-  <footer>333</footer>
+  <header>vitamin</header>
+  <main>
+    <a href="https://www.baidu.com" class="item-container">
+      <img src="~/assets/img/pw_02.jpg" alt="pw" class="item-img" />
+    </a>
+    <a href="https://www.baidu.com" class="item-container">
+      <img src="~/assets/img/pw_02.jpg" alt="pw" class="item-img" />
+    </a>
+    <a href="https://www.baidu.com" class="item-container">
+      <img src="~/assets/img/pw_02.jpg" alt="pw" class="item-img" />
+    </a>
+    <a href="https://www.baidu.com" class="item-container">
+      <img src="~/assets/img/pw_02.jpg" alt="pw" class="item-img" />
+    </a>
+  </main>
+  <footer>footer</footer>
 </template>
+
+<style scoped>
+body {
+  @apply relative bg-amber-500 h-screen
+}
+
+header {
+  @apply absolute top-0 h-10 w-full bg-gray-500 z-50
+}
+
+main {
+  @apply flex h-screen
+}
+
+.item-container {
+  @apply basis-1/4 content-center justify-center overflow-hidden
+}
+
+.item-img {
+  @apply h-screen w-full object-cover transition ease-in-out duration-500 hover:scale-125
+}
+
+footer {
+  @apply absolute bottom-0 h-10 w-full bg-gray-500
+}
+</style>
---
+++ site/tailwind.config.ts
@@ -0,0 +1,13 @@
+export const theme = {
+    extend: {
+        keyframes: {
+            wiggle: {
+                '0%, 100%': {transform: 'rotate(-3deg)'},
+                '50%': {transform: 'rotate(3deg)'},
+            }
+        },
+        animation: {
+            wiggle: 'wiggle 1s ease-in-out infinite',
+        }
+    }
+};
