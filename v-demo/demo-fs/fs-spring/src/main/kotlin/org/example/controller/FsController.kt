3cc59171ef7a36df0011c977b8f782b246757b42
lkzc19 <lkzc19@foxmail.com>
2024-08-01 23:45:17 +0800

    feat: demo-fs

--- v-demo/demo-fs/fs-spring/src/main/kotlin/org/example/controller/FsController.kt
+++ v-demo/demo-fs/fs-spring/src/main/kotlin/org/example/controller/FsController.kt
@@ -72,6 +72,34 @@ class FsController {
     }
 
     /**
+     * 下载文件接口 通过此接口下载可以统计信息
+     * 或者使用 Spring提供的 /file/ 接口下载
+     */
+    @GetMapping("/download")
+    @Throws(IOException::class)
+    fun download(@RequestParam("fullFilename") fullFilename: String, response: HttpServletResponse) {
+        // Content-Disposition 是 HTTP 响应头部的一个字段，它定义了获取的资源如何处理。这个字段通常用于下载操作，告诉浏览器如何处理响应的内容。
+        // Content-Disposition 主要有两个值:
+        //   inline: 这意味着资源应该直接在浏览器中显示，如果可能的话。
+        //      例如，如果资源是一个图像或 PDF 文档，大多数浏览器都能够直接在浏览器窗口中显示这些类型的文件。
+        //   attachment: 这意味着资源应该被下载和保存到本地，而不是直接在浏览器中显示。
+        //
+        //   Content-Disposition 还可以包含一个 filename 参数，这个参数可以指定一个默认的文件名，当用户保存资源时，浏览器会使用这个文件名。
+        //      例如，Content-Disposition: attachment; filename="filename.jpg" 这个头部会告诉浏览器下载资源并将其保存为 "filename.jpg"。
+
+        val file = File(fsDir + File.separator + fullFilename)
+        if (!file.exists()) {
+            throw BizException(message = "[${file.name}]不存在", httpStatus = HttpStatus.BAD_REQUEST)
+        }
+
+        response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(file.name, "UTF-8"))
+        val outputStream = response.outputStream
+        outputStream.write(File(fsDir + File.separator + fullFilename).readBytes()) // 数组是一个字节数组，也就是文件的字节流数组
+        outputStream.flush()
+        outputStream.close()
+    }
+
+    /**
      * 简单文件上传
      */
     @PostMapping("/upload.simple")
@@ -105,33 +133,5 @@ class FsController {
         
         fsService.save(param)
         return param.identifier
-    }
-
-    /**
-     * 下载文件接口 通过此接口下载可以统计信息
-     * 或者使用 Spring提供的 /file/ 接口下载
-     */
-    @GetMapping("/download")
-    @Throws(IOException::class)
-    fun download(@RequestParam("fullFilename") fullFilename: String, response: HttpServletResponse) {
-        // Content-Disposition 是 HTTP 响应头部的一个字段，它定义了获取的资源如何处理。这个字段通常用于下载操作，告诉浏览器如何处理响应的内容。
-        // Content-Disposition 主要有两个值:
-        //   inline: 这意味着资源应该直接在浏览器中显示，如果可能的话。
-        //      例如，如果资源是一个图像或 PDF 文档，大多数浏览器都能够直接在浏览器窗口中显示这些类型的文件。
-        //   attachment: 这意味着资源应该被下载和保存到本地，而不是直接在浏览器中显示。
-        //
-        //   Content-Disposition 还可以包含一个 filename 参数，这个参数可以指定一个默认的文件名，当用户保存资源时，浏览器会使用这个文件名。
-        //      例如，Content-Disposition: attachment; filename="filename.jpg" 这个头部会告诉浏览器下载资源并将其保存为 "filename.jpg"。
-
-        val file = File(fsDir + File.separator + fullFilename)
-        if (!file.exists()) {
-            throw BizException(message = "[${file.name}]不存在", httpStatus = HttpStatus.BAD_REQUEST)
-        }
-
-        response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(file.name, "UTF-8"))
-        val outputStream = response.outputStream
-        outputStream.write(File(fsDir + File.separator + fullFilename).readBytes()) // 数组是一个字节数组，也就是文件的字节流数组
-        outputStream.flush()
-        outputStream.close()
     }
 }
