3c15c3aeea4ee829811f0f78be4b242177cafdcf
lkzc19 <lkzc19@foxmail.com>
2024-08-22 17:56:55 +0800

    feat: sse

--- v-lang/l-kotlin/v-ktor/s-sse/src/main/kotlin/com/example/plugins/Routing.kt
+++ v-lang/l-kotlin/v-ktor/s-sse/src/main/kotlin/com/example/plugins/Routing.kt
@@ -5,15 +5,32 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.sse.*
 import io.ktor.sse.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlin.random.Random
 
 fun Application.configureRouting() {
     install(SSE)
+
+    val notifyChannel = Channel<String>()
+
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        get("/ping") {
+            call.respondText("pong...")
         }
-        sse("/hello") {
-            send(ServerSentEvent("world"))
+
+        get("/send") {
+            val r = Random.nextInt(1000).toString()
+            notifyChannel.send(r)
+            call.respondText(r)
+        }
+
+        sse("/notify") {
+            while (true) {
+                val receive = notifyChannel.receive()
+                send(ServerSentEvent("this is SSE #$receive"))
+                delay(1000)
+            }
         }
     }
 }
