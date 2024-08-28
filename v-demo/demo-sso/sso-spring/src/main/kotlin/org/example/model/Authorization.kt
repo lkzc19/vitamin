1ffab8816184a06d2b179093268ae5bf3cd4160b
lkzc19 <lkzc19@foxmail.com>
2024-08-29 01:04:30 +0800

    feat: sso

---
+++ v-demo/demo-sso/sso-spring/src/main/kotlin/org/example/model/Authorization.kt
@@ -0,0 +1,41 @@
+package org.example.model
+
+import java.time.Instant
+
+
+data class Authorization(
+    val id: String,
+    val registeredClientId: String,
+    val principalName: String,
+    val authorizationGrantType: String,
+    val authorizedScopes: String,
+    val attributes: String,
+    val state: String,
+    val authorizationCodeValue: String,
+    val authorizationCodeIssuedAt: Instant,
+    val authorizationCodeExpiresAt: Instant,
+    val authorizationCodeMetadata: String,
+    val accessTokenValue: String,
+    val accessTokenIssuedAt: Instant,
+    val accessTokenExpiresAt: Instant,
+    val accessTokenMetadata: String,
+    val accessTokenType: String,
+    val accessTokenScopes: String,
+    val refreshTokenValue: String,
+    val refreshTokenIssuedAt: Instant,
+    val refreshTokenExpiresAt: Instant,
+    val refreshTokenMetadata: String,
+    val oidcIdTokenValue: String,
+    val oidcIdTokenIssuedAt: Instant,
+    val oidcIdTokenExpiresAt: Instant,
+    val oidcIdTokenMetadata: String,
+    val oidcIdTokenClaims: String,
+    val userCodeValue: String,
+    val userCodeIssuedAt: Instant,
+    val userCodeExpiresAt: Instant,
+    val userCodeMetadata: String,
+    val deviceCodeValue: String,
+    val deviceCodeIssuedAt: Instant,
+    val deviceCodeExpiresAt: Instant,
+    val deviceCodeMetadata: String,
+)
