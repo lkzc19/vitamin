ad05489ff5c9eac33de18d8955e84ac53cbf47f6
lkzc19 <lkzc19@foxmail.com>
2024-08-29 09:48:56 +0800

    sync

--- v-lang/l-python/v-example/vcode/main.py
+++ v-lang/l-python/v-example/vcode/main.py
@@ -7,6 +7,7 @@ import redis
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-e','--env', type=str, required=False, default="test")
+    parser.add_argument('-k','--key', type=str, required=True)
     args = parser.parse_args()
 
     db = 3
@@ -14,4 +15,10 @@ if __name__ == '__main__':
         db = 6
 
     r = redis.Redis(host='192.168.0.130', port=6379, db=db)
-    r.set('lkzc19', 'nahida')
+
+    key = f'vcode:{args.key}'
+    value = r.get(key)
+    if value == None:
+        print(f'{args.key} 不存在')
+    else:
+        print(value.decode('utf-8'))
