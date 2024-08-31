80a891218077bada092ad8758e016f798258077b
lkzc19 <lkzc19@foxmail.com>
2024-08-31 22:29:52 +0800

    feat: site clarity

--- site/app.vue
+++ site/app.vue
@@ -1,7 +1,13 @@
+<script setup>
+useSeoMeta({
+  title: '饮冰室',
+  description: '十年饮冰，难凉热血。'
+})
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    
+    <NuxtPage />
   </div>
-  <NuxtPage />
 </template>
--- site/nuxt.config.ts
+++ site/nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
+  modules: ['@nuxt/ui', '@nuxt/scripts'],
+
+  scripts: {
+    registry: {
+      clarity: {
+        id: 'nvwq8t6obm'
+      }
+    }
+  }
 })
