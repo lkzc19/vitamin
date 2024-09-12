43a17ebedb42b26ef1a648307438b4f5a6b416b9
lkzc19 <lkzc19@foxmail.com>
2024-09-12 17:11:12 +0800

    feat: py example

--- v-lang/l-python/v-example/data_migration/main.py
+++ v-lang/l-python/v-example/data_migration/main.py
@@ -2,15 +2,16 @@ import argparse
 
 import psycopg2
 from psycopg2.errors import UniqueViolation
+from tqdm import tqdm
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--pg_host', type=str, required=False, default="192.168.0.132")
     parser.add_argument('--pg_port', type=str, required=False, default="5432")
-    parser.add_argument('--pg_dbname', type=str, required=False, default="giakt_test")
-    parser.add_argument('--pg_user', type=str, required=False, default="giakt_test")
-    parser.add_argument('--pg_password', type=str, required=False, default="giakt_test")
+    parser.add_argument('--pg_dbname', type=str, required=False, default="giakt")
+    parser.add_argument('--pg_user', type=str, required=False, default="giakt")
+    parser.add_argument('--pg_password', type=str, required=False, default="giakt")
     args = parser.parse_args()
 
     conn = psycopg2.connect(
@@ -25,35 +26,40 @@ if __name__ == '__main__':
     select = 'SELECT * FROM rc_contacts'
     cur.execute(select)
     rows = cur.fetchall()
-
-    platform_list = []
+            
+    # 找到所有客户与客户之间联系人关系
+    platform_list = [] # 平台人员id
     for row in rows: 
         if row[5] != 'CUSTOMER':
             platform_list.append(row[4])
-
-    contact_list = []
+    customer_list = [] # 客户与客户之间联系人关系
     for row in rows:
         if row[5] == 'CUSTOMER' and row[3] not in platform_list:
-            contact_list.append({'id': row[0], 'ownerId': row[3], 'targetId': row[4], 'role': 'CUSTOMER'})
+            customer_list.append({'id': row[0], 'ownerId': row[3], 'targetId': row[4], 'role': 'CUSTOMER'})
 
-    for it in contact_list:
+    # 联系人迁移
+    print('联系人迁移')
+    bar_format = '{l_bar}{bar} [{n_fmt}/{total_fmt}] {percentage:3.0f}%'
+    for it in tqdm(customer_list, bar_format=bar_format):
+        sql = 'INSERT INTO rc_contacts_v2(owner_id, target_id, role) VALUES (%s, %s, %s)'
         try:
-            sql = 'INSERT INTO rc_contacts_v2(owner_id, target_id, role) VALUES (%s, %s, %s)'
             cur.execute(sql, (it['ownerId'], it['targetId'], it['role']))
             conn.commit()
         except UniqueViolation:
-            pass
-
-    for it in contact_list:
+            conn.rollback()
+    
+    # 联系人标签迁移
+    print('联系人标签迁移')
+    for it in tqdm(rows, bar_format=bar_format):
         sql = 'SELECT t.name FROM rc_tags t INNER JOIN rc_contact_tags ct ON t.id = ct.tag_id WHERE ct.contact_id = %s'
-        cur.execute(sql, (it['id'],))
+        cur.execute(sql, (it[0],))
         tmp_list = cur.fetchall()
         if len(tmp_list) == 0:
             continue
         tag_name_list = [item[0] for item in tmp_list]
+        sql = 'INSERT INTO rc_contacts_tag_v2(owner_id, target_id, tags) VALUES (%s, %s, %s)'
         try:
-            sql = 'INSERT INTO rc_contacts_tag_v2(owner_id, target_id, tags) VALUES (%s, %s, %s)'
-            cur.execute(sql, (it['ownerId'], it['targetId'], tag_name_list))
+            cur.execute(sql, (it[3], it[4], tag_name_list))
             conn.commit()
         except UniqueViolation:
-            pass
+            conn.rollback()
