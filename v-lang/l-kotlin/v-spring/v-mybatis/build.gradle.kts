cb1fef0449b664dc078e84d64cdf56aecb77eeea
lkzc19 <lkzc19@foxmail.com>
2024-09-24 17:47:57 +0800

    sync

--- v-lang/l-kotlin/v-spring/v-mybatis/build.gradle.kts
+++ v-lang/l-kotlin/v-spring/v-mybatis/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.25"
+    kotlin("plugin.noarg") version "1.9.25"
+    kotlin("plugin.allopen") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.3.4"
     id("io.spring.dependency-management") version "1.1.6"
@@ -12,6 +14,14 @@ java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(17)
     }
+}
+
+noArg {
+    annotation("org.example.anno.NoArg")
+}
+
+allOpen {
+    annotation("org.example.anno.AllOpen")
 }
 
 repositories {
---
+++ v-lang/l-kotlin/v-spring/v-mybatis/src/main/kotlin/org/example/model/Ping.kt
@@ -0,0 +1,5 @@
+package org.example.model
+
+data class Ping(
+    var id: Long? = null
+)
