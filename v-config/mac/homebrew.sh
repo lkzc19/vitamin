55ebc391eebf46a1a2b1347ba79f45de1397a6b1
lkzc19 <lkzc19@foxmail.com>
2024-10-04 19:57:29 +0800

    feat: homebrew 安装脚本记录

---
+++ v-config/mac/homebrew.sh
@@ -0,0 +1,7 @@
+# https://gitee.com/cunkai/HomebrewCN
+
+# 安装脚本
+/bin/zsh -c "$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)"
+
+# 卸载脚本
+/bin/zsh -c "$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/HomebrewUninstall.sh)"
