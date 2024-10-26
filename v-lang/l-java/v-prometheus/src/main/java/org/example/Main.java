f66ca869df4b52266ca376a3e8f96c0861ce6c2c
lkzc19 <lkzc19@foxmail.com>
2024-10-26 23:37:54 +0800

    sync

--- v-lang/l-java/v-prometheus/src/main/java/org/example/Main.java
+++ v-lang/l-java/v-prometheus/src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
+
+import io.prometheus.metrics.core.metrics.Counter;
+import io.prometheus.metrics.exporter.httpserver.HTTPServer;
+import io.prometheus.metrics.instrumentation.jvm.JvmMetrics;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException, IOException {
+        JvmMetrics.builder().register(); // initialize the out-of-the-box JVM metrics
+
+        Counter counter = Counter.builder()
+                .name("my_count_total")
+                .help("example counter")
+                .labelNames("status")
+                .register();
+
+        counter.labelValues("ok").inc();
+        counter.labelValues("ok").inc();
+        counter.labelValues("error").inc();
+
+        HTTPServer server = HTTPServer.builder()
+                .port(9400)
+                .buildAndStart();
+
+        System.out.println("HTTPServer listening on port http://localhost:" + server.getPort() + "/metrics");
+
+        Thread.currentThread().join(); // sleep forever
     }
 }
