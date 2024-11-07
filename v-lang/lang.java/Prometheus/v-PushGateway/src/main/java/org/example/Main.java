2e4bcd95009b872c257947bdf4ca85613ef8b959
lkzc19 <lkzc19@foxmail.com>
2024-11-07 17:05:28 +0800

    feat: prometheus alartmanager

--- v-lang/lang.java/Prometheus/v-PushGateway/src/main/java/org/example/Main.java
+++ v-lang/lang.java/Prometheus/v-PushGateway/src/main/java/org/example/Main.java
@@ -3,6 +3,8 @@ package org.example;
 import io.prometheus.metrics.core.metrics.Counter;
 import io.prometheus.metrics.exporter.pushgateway.PushGateway;
 
+import java.util.Random;
+
 public class Main {
 
     private static final PushGateway pushGateway = PushGateway.builder()
@@ -22,10 +24,19 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
+//        push();
         try {
-            counter.labelValues("ok").inc(1);
-            counter.labelValues("fail").inc(2);
+            counter.labelValues("ok").inc(195);
+            counter.labelValues("fail").inc(458);
         } finally {
+            pushGateway.push();
+        }
+    }
+
+    private static void push() throws Exception {
+        while (true) {
+            counter.labelValues("ok").inc(new Random().nextInt(100));
+            counter.labelValues("fail").inc(new Random().nextInt(100));
             pushGateway.push();
         }
     }
