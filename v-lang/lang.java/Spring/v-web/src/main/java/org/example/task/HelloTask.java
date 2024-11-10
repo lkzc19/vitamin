48f08e0e9cb6cdaa6a49c05dbef2c9fe09c78eb0
lkzc19 <lkzc19@foxmail.com>
2024-11-10 22:16:45 +0800

    feat: 定时任务

---
+++ v-lang/lang.java/Spring/v-web/src/main/java/org/example/task/HelloTask.java
@@ -0,0 +1,17 @@
+package org.example.task;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling
+public class HelloTask {
+
+    //每3秒执行一次
+    @Scheduled(cron ="*/6 * * * * ?")
+    private void printHello() {
+        System.out.println("hello");
+    }
+
+}
