79f41af0e47a924edc40eadb3df4c15c8ebcd49b
lkzc19 <lkzc19@foxmail.com>
2025-01-04 23:00:19 +0800

    feat: maven 发布jar包测试

---
+++ v-poc/MavenDeploy/maven.kt/src/test/kotlin/io/github/lkzc19/mk/PrintUtilsTest.kt
@@ -0,0 +1,13 @@
+package io.github.lkzc19.mk
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class PrintUtilsTest {
+
+    @Test
+    fun print() {
+        PrintUtils.print("hello world")
+    }
+}
