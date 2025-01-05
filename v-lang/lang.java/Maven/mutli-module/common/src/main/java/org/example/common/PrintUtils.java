c5e54165ac62b2ead8135f19ace33eccd32bdad0
lkzc19 <lkzc19@foxmail.com>
2025-01-05 20:27:46 +0800

    feat: Maven 测试

---
+++ v-lang/lang.java/Maven/mutli-module/common/src/main/java/org/example/common/PrintUtils.java
@@ -0,0 +1,10 @@
+package org.example.common;
+
+public class PrintUtils {
+
+    private PrintUtils() {}
+
+    public static void print(String message) {
+        System.out.println(message);
+    }
+}
---
+++ v-lang/lang.java/Maven/pkg/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.example.common.PrintUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        PrintUtils.print("Hello World");
+    }
+}
